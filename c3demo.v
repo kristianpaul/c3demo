0607a683d529fe56481bce70fc7ce08a664c54fb
clifford <clifford@2e668e65-2634-0410-9be0-b11ca6b5c640>
2015-10-31 16:38:49 +0000

    ...

--- c3demo.v
+++ c3demo.v
@@ -4,24 +4,33 @@ module c3demo (
 	output reg PANEL_R0, PANEL_G0, PANEL_B0, PANEL_R1, PANEL_G1, PANEL_B1,
 	output reg PANEL_A, PANEL_B, PANEL_C, PANEL_D, PANEL_CLK, PANEL_STB, PANEL_OE
 );
-	reg [9:0] counter = 0;
+	reg [5:0] cnt_x = 0;
+	reg [3:0] cnt_y = 0;
+	reg state = 0;
+
 	always @(posedge clk) begin
-		counter <= counter + 1;
+		if (state == 0) begin
+			if (cnt_x >= 32) begin
+				cnt_x <= 0;
+				cnt_y <= cnt_y + 1;
+			end else begin
+				cnt_x <= cnt_x + 1;
+				PANEL_STB <= 0;
+			end
+			PANEL_CLK <= 0;
+			PANEL_STB <= 0;
+		end else begin
+			PANEL_CLK <= !cnt_x[5];
+			PANEL_STB <= cnt_x[5];
+		end
+		PANEL_OE <= 0;
+		state <= !state;
+	end
 
-		// control signals
-		PANEL_STB <= counter[9];
-		PANEL_CLK <= counter[0];
-		PANEL_OE = 0;
-
-		// select address 0
-		PANEL_A <= 0;
-		PANEL_B <= 0;
-		PANEL_C <= 0;
-		PANEL_D <= 0;
-
-		// select red
-		{PANEL_R1, PANEL_R0} <= 3;
-		{PANEL_G1, PANEL_G0} <= 0;
-		{PANEL_B1, PANEL_B0} <= 0;
+	always @* begin
+		{PANEL_R1, PANEL_R0} = {2{!cnt_y[0]}};
+		{PANEL_G1, PANEL_G0} = {2{!cnt_y[1]}};
+		{PANEL_B1, PANEL_B0} = {2{!cnt_y[2]}};
+		{PANEL_A, PANEL_B, PANEL_C, PANEL_D} = cnt_y;
 	end
 endmodule
