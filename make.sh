1cdf67419e00235f7387381fa5197f146dd9373d
clifford <clifford@2e668e65-2634-0410-9be0-b11ca6b5c640>
2015-11-01 13:15:23 +0000

    ...

--- make.sh
+++ make.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+
 set -ex
+bash firmware.sh
+
 yosys -p 'synth_ice40 -top c3demo -blif c3demo.blif' c3demo.v ledpanel.v picorv32.v
 arachne-pnr -d 8k -p c3demo.pcf -o c3demo.txt c3demo.blif
 icepack c3demo.txt c3demo.bin
