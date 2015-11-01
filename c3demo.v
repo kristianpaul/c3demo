291537c1f536fdc7003e5d902ad01dc700776839
clifford <clifford@2e668e65-2634-0410-9be0-b11ca6b5c640>
2015-11-01 15:14:27 +0000

    ...

--- c3demo.v
+++ c3demo.v
@@ -9,11 +9,69 @@
 // PANEL_[RGB]1 ... color channel for bottom half
 
 module c3demo (
-	input clk,
-	output LED1, LED2, LED3,
+	input CLK12MHZ,
+	output CLKOUT, LED1, LED2, LED3,
 	output PANEL_R0, PANEL_G0, PANEL_B0, PANEL_R1, PANEL_G1, PANEL_B1,
 	output PANEL_A, PANEL_B, PANEL_C, PANEL_D, PANEL_CLK, PANEL_STB, PANEL_OE
 );
+	// 2048 32bit words = 8k bytes memory
+	// 128 32bit words = 512 bytes memory
+	parameter MEM_SIZE = 2048;
+
+	// set to 0 for simulation
+	parameter USE_PLL = 1;
+
+
+	// -------------------------------
+	// PLL
+
+	wire pll_lock, clk;
+	assign CLKOUT = clk;
+
+	generate if (USE_PLL) begin
+		// wire [7:0] DYNAMICDELAY = 0;
+		// wire PLLOUTCORE, EXTFEEDBACK = 0, LATCHINPUTVALUE = 0;
+		// SB_PLL40_CORE #(
+		// 	.FEEDBACK_PATH("SIMPLE"),
+		// 	.DELAY_ADJUSTMENT_MODE_FEEDBACK("FIXED"),
+		// 	.DELAY_ADJUSTMENT_MODE_RELATIVE("FIXED"),
+		// 	.PLLOUT_SELECT("GENCLK"),
+		// 	.FDA_FEEDBACK(0),
+		// 	.FDA_RELATIVE(0),
+		// 	.DIVR(10),
+		// 	.DIVF(0),
+		// 	.DIVQ(1),
+		// 	.FILTER_RANGE(0),
+		// 	.ENABLE_ICEGATE(0),
+		// 	.TEST_MODE(0)
+		// ) uut (
+		// 	.REFERENCECLK   (CLK12MHZ       ),
+		// 	.PLLOUTGLOBAL   (clk            ),
+		// 	.LOCK           (pll_lock       ),
+		// 	.BYPASS         (1'b0           ),
+		// 	.RESETB         (1'b1           ),
+
+		// 	.PLLOUTCORE     (PLLOUTCORE     ),
+		// 	.EXTFEEDBACK    (EXTFEEDBACK    ),
+		// 	.DYNAMICDELAY   (DYNAMICDELAY   ),
+		// 	.LATCHINPUTVALUE(LATCHINPUTVALUE)
+		// );
+
+		reg [7:0] divided_clock;
+		always @(posedge CLK12MHZ)
+			divided_clock <= divided_clock + 1;
+
+		// SB_GB clock_buffer (
+		// 	.USER_SIGNAL_TO_GLOBAL_BUFFER(divided_clock[0]),
+		// 	.GLOBAL_BUFFER_OUTPUT(clk)
+		// );
+
+		assign clk = divided_clock[1];
+	end else begin
+		assign pll_lock = 1, clk = CLK12MHZ;
+	end endgenerate
+
+
 	// -------------------------------
 	// Reset Generator
 
@@ -90,9 +148,6 @@ module c3demo (
 	// -------------------------------
 	// Memory/IO Interface
 
-	// 2048 32bit words = 8k bytes memory
-	// 128 32bit words = 512 bytes memory
-	localparam MEM_SIZE = 128;
 	reg [31:0] memory [0:MEM_SIZE-1];
 	initial $readmemh("firmware.hex", memory);
 
