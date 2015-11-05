8fe6b1edeeea7aadb14ed47c00d396217569e042
clifford <clifford@2e668e65-2634-0410-9be0-b11ca6b5c640>
2015-11-05 15:55:20 +0000

    ...

--- c3demo.v
+++ c3demo.v
@@ -11,8 +11,16 @@
 module c3demo (
 	input CLK12MHZ,
 	output reg DEBUG0, DEBUG1, LED1, LED2, LED3,
+
+	// 32x32 LED Panel
 	output PANEL_R0, PANEL_G0, PANEL_B0, PANEL_R1, PANEL_G1, PANEL_B1,
-	output PANEL_A, PANEL_B, PANEL_C, PANEL_D, PANEL_CLK, PANEL_STB, PANEL_OE
+	output PANEL_A, PANEL_B, PANEL_C, PANEL_D, PANEL_CLK, PANEL_STB, PANEL_OE,
+
+	// RasPi Interface: 9 Data Lines (cmds have MSB set)
+	inout RASPI_11, RASPI_12, RASPI_15, RASPI_16, RASPI_19, RASPI_21, RASPI_24, RASPI_35, RASPI_36,
+
+	// RasPi Interface: Control Lines
+	input RASPI_38, RASPI_40
 );
 	// 2048 32bit words = 8k bytes memory
 	// 128 32bit words = 512 bytes memory
@@ -75,6 +83,93 @@ module c3demo (
 
 
 	// -------------------------------
+	// RasPi Interface PINs
+
+	wire [8:0] raspi_din;
+	reg [8:0] raspi_dout;
+
+	wire raspi_dir = RASPI_38;
+	wire raspi_clk = RASPI_40;
+
+`ifdef TBUF
+	assign {RASPI_11, RASPI_12, RASPI_15, RASPI_16, RASPI_19, RASPI_21, RASPI_24, RASPI_35, RASPI_36} = raspi_dir ? 9'bz : raspi_dout;
+	assign raspi_din = {RASPI_11, RASPI_12, RASPI_15, RASPI_16, RASPI_19, RASPI_21, RASPI_24, RASPI_35, RASPI_36};
+`else
+	SB_IO #(
+		.PIN_TYPE(6'b 1010_01),
+		.PULLUP(1'b 0)
+	) raspi_io [8:0] (
+		.PACKAGE_PIN({RASPI_11, RASPI_12, RASPI_15, RASPI_16, RASPI_19, RASPI_21, RASPI_24, RASPI_35, RASPI_36}),
+		.OUTPUT_ENABLE(!raspi_dir),
+		.D_OUT_0(raspi_dout),
+		.D_IN_0(raspi_din)
+	);
+`endif
+
+
+	// -------------------------------
+	// RasPi Interface Controller
+
+	reg raspi_xfer_r = 0;
+	reg raspi_xfer_d;
+	reg raspi_xfer_q;
+	reg raspi_xfer;
+
+	reg raspi_xfer_dir;
+	reg [8:0] raspi_xfer_din;
+
+	always @(posedge raspi_clk) begin
+		raspi_xfer_din <= raspi_din;
+		raspi_xfer_dir <= raspi_dir;
+	end
+
+	always @(negedge raspi_clk) begin
+		raspi_xfer_r <= !raspi_xfer_r;
+	end
+
+	// clock domain crossing
+	always @(posedge clk) begin
+		raspi_xfer_d <= raspi_xfer_r;
+		raspi_xfer_q <= raspi_xfer_d;
+		raspi_xfer <= raspi_xfer_q;
+	end
+
+	reg [15:0] raspi_mem_addr;
+	reg [31:0] raspi_mem_data;
+	reg [1:0] raspi_mem_state;
+	reg raspi_mem_active;
+
+	reg [3:0] raspi_mode = 0;
+	reg raspi_last_xfer = 0;
+
+	always @(posedge clk) begin
+		raspi_last_xfer <= raspi_xfer;
+		raspi_mem_active <= 0;
+
+		if (raspi_last_xfer != raspi_xfer) begin
+			if (raspi_xfer_dir && raspi_xfer_din[8]) begin
+				raspi_mode <= raspi_xfer_din;
+				raspi_mem_addr <= ~0;
+				raspi_mem_data <= 0;
+				raspi_mem_state <= 0;
+				raspi_dout <= 0;
+			end else
+			case (raspi_mode)
+				0: begin
+					raspi_dout <= ((raspi_xfer_din << 5) + raspi_xfer_din) ^ 7;
+				end
+				1: begin
+					raspi_mem_addr <= raspi_mem_addr + &raspi_mem_state;
+					raspi_mem_data <= {raspi_xfer_din[7:0], raspi_mem_data[31:8]};
+					raspi_mem_state <= raspi_mem_state + 1;
+					raspi_mem_active <= &raspi_mem_state;
+				end
+			endcase
+		end
+	end
+
+
+	// -------------------------------
 	// Reset Generator
 
 	reg [7:0] resetn_counter = 0;
@@ -83,6 +178,8 @@ module c3demo (
 	always @(posedge clk) begin
 		if (!resetn)
 			resetn_counter <= resetn_counter + 1;
+		if (raspi_mode == 1)
+			resetn_counter <= 0;
 	end
 
 
@@ -162,6 +259,10 @@ module c3demo (
 			LED3 <= 0;
 			DEBUG0 <= 0;
 			DEBUG1 <= 0;
+
+			if (raspi_mem_active) begin
+				memory[raspi_mem_addr] <= raspi_mem_data;
+			end
 		end else
 		if (mem_valid && !mem_ready) begin
 			(* parallel_case *)
--- testbench.v
+++ testbench.v
@@ -2,6 +2,11 @@ module testbench;
 	reg clk = 0;
 	integer i, k;
 
+	wire [8:0] raspi_dat;
+	reg [8:0] raspi_dout = 9'bz;
+	reg raspi_dir = 0, raspi_clk = 0;
+	assign raspi_dat = raspi_dout;
+
 	initial begin
 		#5; forever #5 clk = !clk;
 	end
@@ -9,8 +14,50 @@ module testbench;
 	c3demo #(
 		.USE_PLL(0)
 	) uut (
-		.CLK12MHZ(clk)
+		.CLK12MHZ(clk),
+		.RASPI_11(raspi_dat[8]),
+		.RASPI_12(raspi_dat[7]),
+		.RASPI_15(raspi_dat[6]),
+		.RASPI_16(raspi_dat[5]),
+		.RASPI_19(raspi_dat[4]),
+		.RASPI_21(raspi_dat[3]),
+		.RASPI_24(raspi_dat[2]),
+		.RASPI_35(raspi_dat[1]),
+		.RASPI_36(raspi_dat[0]),
+		.RASPI_38(raspi_dir),
+		.RASPI_40(raspi_clk)
 	);
+
+	integer f;
+	reg [31:0] fw;
+
+	task raspi_send;
+		input [8:0] word;
+		begin
+			raspi_dir <= 1;
+			raspi_dout <= word;
+			#50 raspi_clk <= 1;
+			#50 raspi_clk <= 0;
+		end
+	endtask
+
+	initial begin
+		repeat (1000)
+			@(posedge clk);
+
+		f = $fopen("firmware.hex", "r");
+		raspi_send(9'h 101);
+
+		while ($fscanf(f, "%x", fw) == 1) begin
+			raspi_send(fw[ 7: 0]);
+			raspi_send(fw[15: 8]);
+			raspi_send(fw[23:16]);
+			raspi_send(fw[31:24]);
+		end
+
+		raspi_send(9'h 100);
+		$fclose(f);
+	end
 
 	initial begin
 		$dumpfile("testbench.vcd");
